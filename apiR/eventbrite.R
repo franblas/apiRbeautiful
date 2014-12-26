199bf6aa7d23424e4a565695187dbed64450993c
franblas <francois_blas@hotmail.com>
2014-12-26 12:30:47 +0100

    api

--- apiR/eventbrite.R
+++ apiR/eventbrite.R
@@ -13,7 +13,7 @@ library("stringr")
 Eventbrite <- proto(expr={
   
   className <- "Eventbrite"
-  category <- "Music"
+  category <- "Event"
   helpUrl <- "https://developer.eventbrite.com/docs/"
   apiUrl <- "https://www.eventbriteapi.com/v3/"
   apiKey <- ""
@@ -34,6 +34,47 @@ Eventbrite <- proto(expr={
   searchEvents <- function(., text=""){
     text <- .$correctSpace(text)
     str <- paste(apiUrl,"events/search/?token=", .$apiKey, "&q=", text, sep="")
+    x <- as.data.frame(fromJSON(str)$events)
+    x1 <- x$organizer
+    x2 <- x$venue
+    x3 <- x$category
+    x4 <- x$name
+    x5 <- x$description
+    y <- as.data.frame(x$url)
+    y$name <- x4$text
+    y$category <- x3$name
+    y$latitude <- x2$address$latitude
+    y$longitude <- x2$address$longitude
+    y$num_past_events <- x1$num_past_events
+    y$num_future_events <- x1$num_future_events
+    y$description <- x5$text
+    .$nbCall <- .$nbCall + 1
+    return (y)  
+  }
+  
+  getPopularEvents <- function(.){
+    str <- paste(apiUrl,"events/search/?token=", .$apiKey, "&popular=true", sep="")
+    x <- as.data.frame(fromJSON(str)$events)
+    x1 <- x$organizer
+    x2 <- x$venue
+    x3 <- x$category
+    x4 <- x$name
+    x5 <- x$description
+    y <- as.data.frame(x$url)
+    y$name <- x4$text
+    y$category <- x3$name
+    y$latitude <- x2$address$latitude
+    y$longitude <- x2$address$longitude
+    y$num_past_events <- x1$num_past_events
+    y$num_future_events <- x1$num_future_events
+    y$description <- x5$text
+    .$nbCall <- .$nbCall + 1
+    return (y)  
+  }
+  
+  getEventsCity <- function(., text=""){
+    text <- .$correctSpace(text)
+    str <- paste(apiUrl,"events/search/?token=", .$apiKey, "&venue.city=", text, sep="")
     x <- as.data.frame(fromJSON(str)$events)
     x1 <- x$organizer
     x2 <- x$venue
